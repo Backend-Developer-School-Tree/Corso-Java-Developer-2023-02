df915af712a513e272876ceec24111490cc8f68b
Andrea Gasparini <andrea.gasparini11@gmail.com>
2023-06-15 11:14:54 +0200

    Update esempi lambda e stream

--- module_12/src/esempi/lambda_functions/Main.java
+++ module_12/src/esempi/lambda_functions/Main.java
@@ -1,8 +1,6 @@
 package esempi.lambda_functions;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.DoubleToIntFunction;
 import java.util.function.Function;
@@ -63,6 +61,32 @@ public class Main {
 
         System.out.println(mappa);
         System.out.println(mappa2);
+
+        List<Integer> list3 = List.of(3,2,1,4,5,1);
+        Map<Integer, String> mappa3 = list3.stream()
+                .filter(s -> s < 4)
+                .collect(Collectors.toMap(
+                        Function.identity(),    // x -> x
+                        String::valueOf,        // x -> String.valueOf(x)
+                        (x1, x2) -> x1
+                        //TreeMap::new
+                ));
+
+        System.out.println("mappa3: " + mappa3);
+
+        List<String> list4 = List.of("andrea", "marco", "davide", "lavinia");
+        Map<Integer, List<String>> mappa4 = list4.stream()
+                // .collect(Collectors.groupingBy(x -> x.length()));
+                .collect(Collectors.groupingBy(String::length));
+
+        System.out.println(mappa4);
+
+
+        System.out.println(list4.stream()
+                .collect(Collectors.joining(";")));
+
+        System.out.println(list4.stream()
+                .reduce((x1, x2) -> x1+";"+x2)); // ritorna un Optional se non specifichiamo un accumulatore come primo parametro
 
 
 
